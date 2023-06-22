bdc54526d989c6acc91ebf6de45259aacb9e5dcd
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-06-22 16:40:42 +0800

    Add user signup functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
-const UserValidator = require('../validators/user') 
+const UserValidator = require('../validators/user')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
-module.exports.login = (req,res) => {
+module.exports.login = (req, res) => {
     const { error } = UserValidator.loginSchema.validate(req, {
         // Setting abortEarly to true won't check other fields in case of error
         // If email is incorrect, password won't be checked anymore
@@ -12,11 +14,22 @@ module.exports.login = (req,res) => {
     return res.send("Logged in successfully");
 }
 
-module.exports.signup = (req,res) => {
-    const { error } = UserValidator.signupSchema.validate(req, {
+module.exports.signup = async (req, res) => {
+    const { error } = UserValidator.signupSchema.validate(req.body, {
         abortEarly: false,
     });
-
     if (error) return res.status(400).send(error.details[0]);
-    return res.send("Signed up successfully");
+
+    const isUsernameAlreadyExists = await prisma.user.findFirst() !== null
+    if (isUsernameAlreadyExists) return res.status(409).send("Username already exists.")
+
+    await prisma.user.create({
+        data: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        }
+    })
+
+    return res.status(200).send("Signed up successfully");
 }
