e794174ab916868366f8a6d379738f90e9613a20
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-06-23 14:07:15 +0800

    Add login function and make bcrypt synchronous

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,15 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
 
-module.exports.login = (req, res) => {
-    const { error } = UserValidator.loginSchema.validate(req, { abortEarly: false });
+module.exports.login = async (req, res) => {
+    const { error } = UserValidator.loginSchema.validate(req.body, { abortEarly: false });
     if (error) return res.status(400).send(error.details[0]);
+
+    const user = await prisma.user.findFirst({ where: { email: req.body.email } })
+    if (!user) return res.status(403).send('Incorrect email or password')
+
+    const isPasswordMatching = bcrypt.compareSync(req.body.password, user.password)
+    if (!isPasswordMatching) return res.status(403).send('Incorrect email or password')
 
     return res.send("Logged in successfully");
 }
@@ -14,10 +20,10 @@ module.exports.signup = async (req, res) => {
     const { error } = UserValidator.signupSchema.validate(req.body, { abortEarly: false });
     if (error) return res.status(400).send(error.details[0]);
 
-    const isUsernameAlreadyExists = checkIfUsernameExistsAlready(req.body)
+    const isUsernameAlreadyExists = await checkIfUsernameExistsAlready(req.body)
     if (isUsernameAlreadyExists) return res.status(409).send("Username already exists.")
 
-    const isEmailAlreadyExists = checkIfEmailExistsAlready(req.body)
+    const isEmailAlreadyExists = await checkIfEmailExistsAlready(req.body)
     if (isEmailAlreadyExists) return res.status(409).send("Email already exists, please login to your account.")
 
     const SALT_ROUNDS = 10
@@ -25,11 +31,10 @@ module.exports.signup = async (req, res) => {
         data: {
             username: req.body.username,
             email: req.body.email,
-            password: await bcrypt.hash(req.body.password, SALT_ROUNDS)
+            password: bcrypt.hashSync(req.body.password, SALT_ROUNDS)
         }
     }
     await prisma.user.create(HASHED_DATA)
-
     return res.status(200).send("Signed up successfully");
 }
 
