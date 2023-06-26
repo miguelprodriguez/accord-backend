96d166bbef70c3507a6970b020b4202df8189bea
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-06-26 18:34:07 +0800

    Add session upon signup and login

--- controllers/user.js
+++ controllers/user.js
@@ -3,13 +3,20 @@ const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
 
 module.exports.login = async (req, res) => {
-    const user = await prisma.user.findFirst({ where: { email: req.body.email } })
-    if (!user) return res.status(403).send('Incorrect email or password')
+    const existingUser = await prisma.user.findFirst({ where: { email: req.body.email } })
+    if (!existingUser) return res.status(403).send('Incorrect email or password')
 
-    const isPasswordMatching = bcrypt.compareSync(req.body.password, user.password)
+    const isPasswordMatching = bcrypt.compareSync(req.body.password, existingUser.password)
     if (!isPasswordMatching) return res.status(403).send('Incorrect email or password')
 
-    return res.status(200).send("Logged in successfully");
+    const userId = await getUserId(req.body)
+    const sessionData = {
+        email: req.body.email,
+        id: userId
+    }
+    req.session.user = sessionData
+
+    return res.status(200).send("Logged in successfully")
 }
 
 module.exports.signup = async (req, res) => {
@@ -28,7 +35,15 @@ module.exports.signup = async (req, res) => {
         }
     }
     await prisma.user.create(HASHED_DATA)
-    return res.status(200).send("Signed up successfully");
+
+    const userId = await getUserId(req.body)
+    const sessionData = {
+        email: req.body.email,
+        userId
+    }
+    req.session.user = sessionData
+
+    return res.send(req.session.user);
 }
 
 const checkIfUsernameExistsAlready = async (requestBody) => {
@@ -36,5 +51,14 @@ const checkIfUsernameExistsAlready = async (requestBody) => {
 }
 
 const checkIfEmailExistsAlready = async (requestBody) => {
-    return await prisma.user.findFirst({ where: { username: requestBody.email } }) !== null
+    return await prisma.user.findFirst({ where: { email: requestBody.email } }) !== null
+}
+
+const getUserId = async (requestBody) => {
+    const result = await prisma.user.findFirst({
+        where: { email: requestBody.email },
+        select: { id: true }
+    })
+    
+    return result.id
 }
--- index.js
+++ index.js
@@ -6,30 +6,38 @@ const server = http.createServer(app)
 const helmet = require('helmet')
 const cors = require('cors')
 const userRoutes = require('./routes/user')
+const session = require('express-session')
+require('dotenv').config()
 
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        credentials: true
-    }
-})
+const corsConfig = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+}
 
-// Parse req body; middleware functions
+const io = new Server(server, { cors: corsConfig })
+
+// Middleware functions
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet())
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    credentials: true
+app.use(cors(corsConfig))
+
+app.use(session({
+    name: 'sid', // session ID
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+    },
 }))
 
 // Routes
 app.use('/api/users', userRoutes);
-
 app.get('/', (req, res) => {
     res.json("hi")
 })
 
-io.on('connection', socket => {})
+io.on('connection', socket => { })
 
 server.listen(4000, () => console.log('Server listening on port 4000'))
