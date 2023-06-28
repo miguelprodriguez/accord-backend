763bc3865922ffd51fe4e5e5c170adbf1e15a5d8
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-06-28 15:36:03 +0800

    Add try catch for error handling

--- controllers/user.js
+++ controllers/user.js
@@ -3,47 +3,55 @@ const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
 
 module.exports.login = async (req, res) => {
-    const existingUser = await prisma.user.findFirst({ where: { email: req.body.email } })
-    if (!existingUser) return res.status(403).send({ message: 'Incorrect email or password' })
+    try {
+        const existingUser = await prisma.user.findFirst({ where: { email: req.body.email } })
+        if (!existingUser) return res.status(403).send({ message: 'Incorrect email or password' })
 
-    const isPasswordMatching = bcrypt.compareSync(req.body.password, existingUser.password)
-    if (!isPasswordMatching) return res.status(403).send({ message: 'Incorrect email or password' })
+        const isPasswordMatching = bcrypt.compareSync(req.body.password, existingUser.password)
+        if (!isPasswordMatching) return res.status(403).send({ message: 'Incorrect email or password' })
 
-    const userId = await getUserId(req.body)
-    const sessionData = {
-        email: req.body.email,
-        id: userId
+        const userId = await getUserId(req.body)
+        const sessionData = {
+            email: req.body.email,
+            id: userId
+        }
+        req.session.user = sessionData
+
+        return res.status(200).send({ message: "Logged in successfully" })
+    } catch (error) {
+        return res.status(500).send({ message: error.message })
     }
-    req.session.user = sessionData
-
-    return res.status(200).send("Logged in successfully")
 }
 
 module.exports.signup = async (req, res) => {
-    const isUsernameAlreadyExists = await checkIfUsernameExistsAlready(req.body)
-    if (isUsernameAlreadyExists) return res.status(409).send({ message: "Username already exists." })
+    try {
+        const isUsernameAlreadyExists = await checkIfUsernameExistsAlready(req.body)
+        if (isUsernameAlreadyExists) return res.status(409).send({ message: "Username already exists." })
 
-    const isEmailAlreadyExists = await checkIfEmailExistsAlready(req.body)
-    if (isEmailAlreadyExists) return res.status(409).json({ message: "Email already exists." })
+        const isEmailAlreadyExists = await checkIfEmailExistsAlready(req.body)
+        if (isEmailAlreadyExists) return res.status(409).json({ message: "Email already exists." })
 
-    const SALT_ROUNDS = 10
-    const HASHED_DATA = {
-        data: {
-            username: req.body.username,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, SALT_ROUNDS)
+        const SALT_ROUNDS = 10
+        const HASHED_DATA = {
+            data: {
+                username: req.body.username,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, SALT_ROUNDS)
+            }
         }
-    }
-    await prisma.user.create(HASHED_DATA)
+        await prisma.user.create(HASHED_DATA)
 
-    const userId = await getUserId(req.body)
-    const sessionData = {
-        email: req.body.email,
-        userId
-    }
-    req.session.user = sessionData
+        const userId = await getUserId(req.body)
+        const sessionData = {
+            email: req.body.email,
+            userId
+        }
+        req.session.user = sessionData
 
-    return res.status(200).send({ message: "User has been created successfully." });
+        return res.status(200).send({ message: "User has been created successfully." });
+    } catch (error) {
+        return res.status(500).send({ message: error.message })
+    }
 }
 
 const checkIfUsernameExistsAlready = async (requestBody) => {
