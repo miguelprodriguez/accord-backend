d8614c5edceac6e69e6dc49a959e8cd83e0b88f1
Miguel Rodriguez <mglprodriguez@gmail.com>
2023-07-18 16:04:38 +0800

    Set userId on redis store

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require('helmet')
 const cors = require('cors')
 const userRoutes = require('./routes/user')
 const { sessionMiddleware, wrap } = require('./middlewares/session')
+const Redis = require('ioredis')
 
 const corsConfig = {
     origin: 'http://localhost:3000',
@@ -30,14 +31,22 @@ app.get('/', (req, res) => {
 const io = new Server(server, { cors: corsConfig })
 
 io.use(wrap(sessionMiddleware))
+
+const redisClient = new Redis()
 io.use((socket, next) => {
     if (!socket.request.session || !socket.request.session.user) {
         next(new Error("You shall not pass"))
+        return
     }
-    else next()
+
+    // Create user property and desctruce from socket request
+    socket.user = { ...socket.request.session.user }
+    next()
 })
 io.on('connection', socket => {
-    console.log("Socket request: ", socket.request.session)
+    console.log("Socket username: ", socket.user)
+    // HSET key field value [field value ...]
+    redisClient.hset(`userId: ${socket.user.username}`, "userId", "socket.user.userId")
 })
 
 server.listen(4000, () => console.log('Server listening on port 4000'))
